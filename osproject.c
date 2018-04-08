1f952a74a629924fc602d45a18fcbddeeec8e2b2
AMIT KUMAR RANJAN <akranjan2307@gmail.com>
2018-04-08 19:06:14 +0530

    fourth commit

--- osproject.c
+++ osproject.c
@@ -1,28 +1,39 @@
+//Operating System Project By Amit Kumar Ranjan
+//Section : K1603
+//Registration NO. : 11607646
+
 #include<stdio.h>
 #include<stdbool.h>
 //average time
 
-void AvgTime(int process[], int n, int brust[],int quant)
+void AvgTime(int process[], int n, int brust[],int quant,char name)
 {
     int i,wt[n], temp[n], total_wt = 0, total_time= 0;
     int avg_wait,avg_turnaround;
     WaitingTime(process, n,brust, wt, quant);
     TurnAroundTime(process, n, brust, wt, temp);
-    printf("Processes : Burst time :  Waiting time :   Turn around time : \n");
+    printf("Processes :  Arival time :  Burst time :  Waiting time :   Turn around time : \n");
 
-    for (i=0; i<n; i++)
+    for (i=1; i<=n; i++)
     {
         total_wt = total_wt + wt[i];
         total_time = total_time + temp[i];
-        printf("%d \t\t  %d \t \t %d \t\t\t%d\n",i+1,brust[i],wt[i],temp[i]);
+        printf("%d \t\t  %d \t\t  %d \t \t %d \t\t\t%d\n",i,process[i],brust[i],wt[i],temp[i]);
     }
 
     // average query time and average waiting time
     avg_wait=total_wt/n;
     avg_turnaround=total_time/n;
-    
-    printf("\n Time he spend on handling query is : %d",avg_turnaround);
-    printf("\n Average query time is : %d",avg_wait);
+    if(name=='s')
+    {
+	printf("\n Time he spend on handling of students query is : %d",avg_turnaround);
+        printf("\n Average query time is : %d",avg_wait);
+    }
+    else
+    {
+	printf("\n Time he spend on handling of faculty query is : %d",avg_turnaround);
+        printf("\n Average query time is : %d",avg_wait);
+    }
 }
 
 //waiting time frome here
@@ -31,14 +42,14 @@ void WaitingTime(int process[], int n,int brust[], int wt[], int quan)
 {
     int i,t,rem[n];
     t=0;
-    for (i = 0 ; i < n ; i++)
+    for (i = 1 ; i <= n ; i++)
         {
             rem[i] =  brust[i];
         }
     while (1)
     {
         bool flag= true;
-        for (i = 0 ; i < n; i++)
+        for (i = 1; i <= n; i++)
         {
             if (rem[i] > 0)
             {
@@ -51,8 +62,9 @@ void WaitingTime(int process[], int n,int brust[], int wt[], int quan)
                 }
                 else
                 {
+	            wt[i] = t - process[i];
                     t = t + rem[i];
-                    wt[i] = t - brust[i];
+                    //wt[i] = t - brust[i];
                     rem[i] = 0;
                 }
             }
@@ -67,7 +79,7 @@ void WaitingTime(int process[], int n,int brust[], int wt[], int quan)
 void TurnAroundTime(int process[], int n,int brust[], int wt[], int temp[])
 {
     int i;
-    for (i = 0; i < n ; i++)
+    for (i = 1; i <=n ; i++)
         temp[i] = brust[i] + wt[i];
 }
 
@@ -119,11 +131,12 @@ int main()
 	scanf("%d",&burst_time1[i]);
         printf("\n");
     }
-    printf("TIME QUANTUM USED IS 5 :\n");
+    printf("TIME QUANTUM FOR QUEUE IS 10 :\n");
+    printf("TIME QUANTUM USED process inside queue IS 5 :\n\n");
     int quantum = 5;
-    AvgTime(process, n, burst_time, quantum); 
+    AvgTime(process, n, burst_time, quantum,'s'); 
     n = sizeof process1 / sizeof process1[0];
     printf("\n\n\n");
-    AvgTime(process1, n ,burst_time1,quantum);
+    AvgTime(process1, n ,burst_time1,quantum,'f');
     return 0;
 }
